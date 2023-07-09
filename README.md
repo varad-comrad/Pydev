663a0330e7f08e1973fe3ac3dc7e21a42f394ff0
varad-comrad <fabricio_asfora_002@hotmail.com>
2023-07-09 12:30:44 -0300

    baselines asyncwrapper determined

---
+++ README.md
@@ -0,0 +1,5 @@
+Pydev
+
+Work In Progress
+
+still undefined
--- asyncwrapper.py
+++ asyncwrapper.py
@@ -13,10 +13,34 @@ class TaskWrapper:
     def __await__(self):
         pass
 
+
+# USAGE:
+
 def other_tasks():
+    pass
+
+def some_tasks(load_result):
     pass
 
 async def load():
     with await TaskWrapper() as wrapper:
-        pass
-    other_tasks()
+        some_tasks(wrapper.results)
+    other_tasks() # other_tasks is executed until wrapper gets a result. Then the code inside context manager is executed
+                  # If load() terminates before wrapper, the termination is halted until the context manager terminates
+
+# EQUIVALENT TO:
+
+# def other_tasks():
+#     pass
+
+# def some_tasks(loaded_result):
+#     pass
+
+# async def load_result():
+#     pass
+
+# async def load():
+#     task = asyncio.create_task(load_result())
+#     other_tasks()
+#     await asyncio.gather(task)
+#     some_tasks(task.result())
